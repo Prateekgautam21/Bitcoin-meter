0e346009a1cc735936a8ead9dcab6709ea6e611d
Prateekgautam21 <gautamprateek22@gmail.com>
2019-11-08 13:41:26 +0530

    updated Ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import axios from 'axios';
 import Table from './Components/Table';
 
 class App extends Component{
@@ -8,77 +7,14 @@ class App extends Component{
       constructor(props) {
         super(props);
 
-        this.state = {
-          currentvalueofbitcoin: {},
-          previousvalueofbitcoin: {},
-        }
-
       }
-
-      componentDidMount(){
-        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-        .then(response => {
-          this.setState({
-            previousvalueofbitcoin: this.state.currentvalueofbitcoin,
-            currentvalueofbitcoin: {currentvalueofbitcoin: response.data}
-          })
-        })
-      }
-
-      // valueUpgrader = () => {
-      //   this.setState(prevState => ({
-      //     previousvalueofbitcoin: prevState
-      //   })
-      //   );
-
-      //   console.log('Valueupgrader Ran');        
-      //   console.log(this.state.previousvalueofbitcoin);
-      //   console.log('Valueupgrader Ran');
-      // }
 
       render(){
         
-        // console.log('render ran');
-        // console.log(this.state.previousvalueofbitcoin);
-        // // console.log(this.state.previousvalueofbitcoin.currentvalueofbitcoin.bpi.EUR.rate);
-        // // console.log(this.state);
-        // console.log('render ran');
-        
-        var previousvalueofbitcoin = this.state.previousvalueofbitcoin
-        // console.log(previousvalueofbitcoin);
-        
-        var {currentvalueofbitcoin} = this.state.currentvalueofbitcoin;
-
-        let eurRate = currentvalueofbitcoin ? currentvalueofbitcoin.bpi.EUR.rate: ''
-        let gbpRate = currentvalueofbitcoin ? currentvalueofbitcoin.bpi.GBP.rate: ''
-        let usdRate = currentvalueofbitcoin ? currentvalueofbitcoin.bpi.USD.rate: ''
-        
-        
-
-        for(var key in previousvalueofbitcoin) {
-          if(key == "currentvalueofbitcoin"){
-            // console.log(previousvalueofbitcoin.currentvalueofbitcoin.bpi.EUR.rate)
-            if(eurRate != eurPrevRate){
-              var eurPrevRate =  previousvalueofbitcoin.currentvalueofbitcoin.bpi.EUR.rate;
-              var gbpPrevRate =  previousvalueofbitcoin.currentvalueofbitcoin.bpi.GBP.rate;
-              var usdPrevRate =  previousvalueofbitcoin.currentvalueofbitcoin.bpi.USD.rate;
-            }
-            
-    
-          }
-          
-         }
-
-        console.log(eurPrevRate);
-        console.log(gbpPrevRate);
-        console.log(usdPrevRate);
-        console.log("----------------")
-
         return (
           <div className="App">
             <h1>Bitcoin Monitor</h1>
-            <Table prevE={eurPrevRate} prevG={gbpPrevRate} prevU={usdPrevRate} eurRate={eurRate} gbpRate={gbpRate} usdRate={usdRate} />
-            <button onClick={this.componentDidMount.bind(this)}>Refresh</button>
+            <Table />
           </div>
           )
         }
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,42 +1,109 @@
 import React, { Component } from 'react'
 import "./Table.css";
+import listofcodes from "./supportedCountries";
+import axios from 'axios'
 
 class Table extends Component {
-    render() {
 
-        let {eurRate, gbpRate, usdRate,prevE,prevG,prevU} = this.props;
+        constructor(props) {
+          super(props);
         
-        return (
-            <div>
-            <table>
-            <thead>
-              <tr> 
-                <th>      </th>
-                <th>Before</th>
-                <th>Current</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Eur</td>
-                <td>{prevE}</td>
-                <td>{eurRate}</td>
-              </tr>
-              <tr>
-                <td>Pnd</td>
-                <td>{prevG}</td>
-                <td>{gbpRate}</td>
-              </tr>
-              <tr>
-                <td>Usd</td>
-                <td>{prevU}</td>
-                <td>{usdRate}</td>
-              </tr>
-            </tbody>
-          </table>
-            </div>
-        )
-    }
+          this.state = {
+            currentspecificvalueofbitcoin: null,
+            previousspecificvalueofbitcoin: null,
+            previousvalueofbitcoin: {},
+            currentvalueofbitcoin: {}
+          }
+
+        }
+
+        componentDidMount(){
+              
+          axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(response => {
+              this.setState({
+                previousvalueofbitcoin: this.state.currentvalueofbitcoin,
+                currentvalueofbitcoin: {currentvalueofbitcoin: response.data}
+              })
+            });
+          if(this.state.currentspecificvalueofbitcoin != null){
+            this.countrycodepricegetter()
+          }
+        }
+
+        countrycodepricegetter = () => {
+              var countryCode = this.refs.countrycodetype.value;
+              axios.get(`https://api.coindesk.com/v1/bpi/currentprice/${countryCode}.json`)
+              .then(response => {
+                  console.log(response.data);
+                  this.setState({
+                      previousspecificvalueofbitcoin: this.state.currentspecificvalueofbitcoin,
+                      currentspecificvalueofbitcoin : response.data.bpi[countryCode].rate
+                  })
+              })
+        }
+        
+        render() {  
+                var previousvalueofbitcoin = this.state.previousvalueofbitcoin
+              
+                var {currentvalueofbitcoin} = this.state.currentvalueofbitcoin;
+        
+                let eurRate = currentvalueofbitcoin ? currentvalueofbitcoin.bpi.EUR.rate: ''
+                let gbpRate = currentvalueofbitcoin ? currentvalueofbitcoin.bpi.GBP.rate: ''
+                let usdRate = currentvalueofbitcoin ? currentvalueofbitcoin.bpi.USD.rate: ''
+                
+                for(var key in previousvalueofbitcoin) {
+                  if(key == "currentvalueofbitcoin"){
+                    if(eurRate != eurPrevRate){
+                      var eurPrevRate =  previousvalueofbitcoin.currentvalueofbitcoin.bpi.EUR.rate;
+                      var gbpPrevRate =  previousvalueofbitcoin.currentvalueofbitcoin.bpi.GBP.rate;
+                      var usdPrevRate =  previousvalueofbitcoin.currentvalueofbitcoin.bpi.USD.rate;
+                    }
+                  }
+                }
+
+                return (
+                    <div>
+                    <table>
+                    <thead>
+                      <tr> 
+                        <th>      </th>
+                        <th>Before</th>
+                        <th>Current</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Eur</td>
+                        <td>{eurPrevRate}</td>
+                        <td>{eurRate}</td>
+                      </tr>
+                      <tr>
+                        <td>Pnd</td>
+                        <td>{gbpPrevRate}</td>
+                        <td>{gbpRate}</td>
+                      </tr>
+                      <tr>
+                        <td>Usd</td>
+                        <td>{usdPrevRate}</td>
+                        <td>{usdRate}</td>
+                      </tr>
+                      <tr>
+                        <td><select ref="countrycodetype" onChange={this.countrycodepricegetter.bind(this)}>
+                        <option value=""></option>
+                        {listofcodes.map((code, index) => {
+                            return <option key={index} value={code}>{code}</option>
+                        })}
+                        </select></td>
+                        <td>{this.state.previousspecificvalueofbitcoin}</td>
+                        <td>{this.state.currentspecificvalueofbitcoin}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <button onClick={this.componentDidMount.bind(this)}>Refresh</button>
+                    </div>
+                )
+            }
 }
 
 export default Table
